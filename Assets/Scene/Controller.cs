bbb8c00eb1069a234701c1c8f86688598fa9c8b7
mark-quickel <mark.quickel@live.com>
2023-06-26 15:37:20 -0400

    Branch commit 02. squad basic follow with navmesh

--- Assets/Scene/Controller.cs
+++ Assets/Scene/Controller.cs
@@ -1,28 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pik.Pik;
 
 namespace Pik.Scene { 
     public class Controller : MonoBehaviour
     {
         public GameObject Pik;
         public GameObject Captain;
-        public GameObject PikParent;
-        private const int maxPik = 99;
+        public Transform PikParent;
+        public Transform PikNavTarget;
+        
+        private const int maxPik = 49;
+        private readonly List<GameObject> pikList = new();
+        
 
         // Start is called before the first frame update
         void Start()
         {
+            // Create Pik with some randomized attribtues (for now)
             for (var i = 0; i < maxPik; i++)
             {
-                GameObject.Instantiate(Pik, new Vector3(Random.Range(0,2.0f), 0, Random.Range(0, 2.0f)), Quaternion.identity , PikParent.transform);
+                var o = new Vector3(Random.Range(0.0f, 2.0f), 0, Random.Range(0.0f, 2.0f));
+                var p = Instantiate(Pik, PikNavTarget.position, Quaternion.identity, PikParent);
+                pikList.Add(p);
+                var m = p.GetComponent<Move>();
+                m.Target = PikNavTarget;
+                m.Offset = o;
+                m.Speed = Random.Range(3.0f, 3.5f);
+
             }
         }
 
         // Update is called once per frame
         void Update()
         {
-        
         }
     }
 }
