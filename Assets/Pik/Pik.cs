7b037fb811347f6238b63d59f2e9811b33afde1a
Mark Quickel <112185610+mark-quickel@users.noreply.github.com>
2023-08-14 10:15:19 -0400

    Branch commit 04. throwing and whistling basics 1 of 2

--- Assets/Pik/Pik.cs
+++ Assets/Pik/Pik.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,21 +12,55 @@ namespace Pik.Pik
         public float Acceleration = 2.0f;
         public Transform Target;
         public Vector3 Offset;
+        public bool IsAttended;
 
-        private NavMeshAgent NMAgent;
+        public EventHandler PikMoved;
+        public EventHandler PikThrown;
+        public EventHandler PikCalled;
+
+        private NavMeshAgent NavMeshAgentInstance;
+        private Rigidbody RigidbodyInstance;
+        
 
         void Start()
         {
-            NMAgent = GetComponent<NavMeshAgent>();
-            NMAgent.acceleration = Acceleration;
-            NMAgent.speed = Speed;
-        }
+            NavMeshAgentInstance = GetComponent<NavMeshAgent>();
+            NavMeshAgentInstance.acceleration = Acceleration;
+            NavMeshAgentInstance.speed = Speed;
 
-        // Update is called once per frame
+            RigidbodyInstance = GetComponent<Rigidbody>();
+
+            IsAttended = true;
+        }
+        
         void Update()
         {
-            NMAgent.destination = Target.position + Offset;
+            Move();
         }
+
+        public void Throw(Vector3 thrower, Vector3 direction)
+        {
+            IsAttended = false;
+            transform.position = thrower;
+            NavMeshAgentInstance.enabled = false;
+            RigidbodyInstance.AddForce(direction, ForceMode.Impulse);
+            PikThrown?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Call()
+        {
+            // TODO: check to see if grounded
+            NavMeshAgentInstance.enabled = true;
+            IsAttended = true;
+        }
+        
+        void Move()
+        {
+            if (!IsAttended) return;
+            NavMeshAgentInstance.destination = Target.position + Offset;
+            PikMoved?.Invoke(this, EventArgs.Empty);
+        }
+
 
     }
 }
